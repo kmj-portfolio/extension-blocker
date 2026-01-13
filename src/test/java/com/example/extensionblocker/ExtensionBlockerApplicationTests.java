a488a8fe60ee6be6bf723bfdcb02098caaba0e72
miji <mjkim2304@gmail.com>
2026-01-14 01:06:03 +0900

    Initial commit

---
+++ src/test/java/com/example/extensionblocker/ExtensionBlockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.extensionblocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtensionBlockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
