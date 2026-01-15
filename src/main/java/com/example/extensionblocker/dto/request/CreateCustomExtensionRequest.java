74e412fa2f82bd9c3f9406484cc8ad6c5e5ae07a
miji <mjkim2304@gmail.com>
2026-01-15 21:44:28 +0900

    chore: CreateCustomExtensionRequest swagger 오타 수정

--- src/main/java/com/example/extensionblocker/dto/request/CreateCustomExtensionRequest.java
+++ src/main/java/com/example/extensionblocker/dto/request/CreateCustomExtensionRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Size;
 
 @Schema(description = "커스텀 확장자 생성 요청")
 public record CreateCustomExtensionRequest(
-        @Schema(description = "확장자 이름에는 영문, 숫자, -. _가 허용됩니다.")
+        @Schema(description = "확장자 이름에는 영문, 숫자, -, _가 허용됩니다.")
         @NotBlank(message = "공백이 아닌 확장자 이름을 입력해주세요.")
         @Size(min = 1, max = 20, message = "확장자 이름은 최대 20자 까지 입력 가능하니다.")
         String extensionName
